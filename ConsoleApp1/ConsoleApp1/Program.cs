dba0a450d1da179fdec45b57136f3443117b9622
Kamil Kępka <kamil.kepka08072002@gmail.com>
2022-01-26 10:20:54 +0100

    added writeline

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("pozdro 600");
             Console.WriteLine("Hello World!");
         }
     }
